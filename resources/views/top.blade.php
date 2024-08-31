42f354b9ec62776598a0177c7de553066ffa97fc
DokuDokuringo <153196181+Ringo0803Ringo@users.noreply.github.com>
2024-08-31 10:43:01 +0900

    top画面の作成

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,5 +2,12 @@
 
 @section('content')
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="text-center">星占い</h2>
+        </div>
+    </div>
+</div>
 
 @endsection
