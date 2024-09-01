bc37550587b7bdc208811316d77c212d5f34dba4
DokuDokuringo <153196181+Ringo0803Ringo@users.noreply.github.com>
2024-09-01 18:15:58 +0900

    ログイン機能作成

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-md fixed-top custom-navbar">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.display_name') }}
+                {{ config('app.name', 'Laravel') }}
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
@@ -11,7 +11,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav me-auto">
-                    <i class="fa-solid fa-wine-bottle fa-2x"></i>
+                    
                 </ul>
 
                 <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,9 +23,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/user/{user}', [UserController::class, 'user'])->name('user');
 });
 
 require __DIR__.'/auth.php';
