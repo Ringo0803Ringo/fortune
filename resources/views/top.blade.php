af75efa88000d2a5b46c8282710d2912aa0893bb
DokuDokuringo <153196181+Ringo0803Ringo@users.noreply.github.com>
2024-09-26 22:05:11 +0900

    Revert "運勢表示の位置を変更"
    This reverts commit 0bf60d05ec4e41b153d48c49b1b33d73c99c76de.

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -7,7 +7,7 @@
         <div class="col-md-12">
             <h2 class="text-center">星占い</h2>
             <h3 class="text-center">今日の運勢</h3>
-            <p class="text-center mt-4">結果：{{ $result }}</p>
+            <p>結果：{{ $result }}</p>
         </div>
     </div>
 </div>
